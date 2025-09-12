875019fc9fd17868068edbc1bfaf50d83a88df84
Oliver Tacke <o.tacke@posteo.de>
2025-09-12 16:46:41 +0200

    Update composer reference

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'ndlano/h5p-caretaker',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '82ec3a4d1eb7d657a04ce209d8df61d6ae2cc205',
+        'reference' => '980cec2c5847471de3035aad7fa60142d8112696',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'ndlano/h5p-caretaker' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '82ec3a4d1eb7d657a04ce209d8df61d6ae2cc205',
+            'reference' => '980cec2c5847471de3035aad7fa60142d8112696',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
